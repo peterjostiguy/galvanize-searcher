2324a490200d2f01d8a1df3b58859917aef9b814
Peter Ostiguy <peterjostiguy@gmail.com>
2018-04-18 10:15:15 -0600

    Update 3.js

---
+++ exercises/3.js
@@ -0,0 +1,8 @@
+/* Return the name of the person using the desiredProperty variable*/
+
+const desiredProperty = "name";
+const person = {name: "Kyle"};
+
+module.exports = function(){
+    // Your code here
+}
